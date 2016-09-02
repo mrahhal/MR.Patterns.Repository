531f454fb8a5d9567914cbed23dd02a6f70d0bc5
Mohammad Rahhal <m.r992@hotmail.com>
2016-09-02 11:42:45 +0300

    + more overloads to RunInTransactionAsync

--- src/MR.Patterns.Repository/IRepositoryCore.Default.cs
+++ src/MR.Patterns.Repository/IRepositoryCore.Default.cs
@@ -40,53 +40,103 @@ namespace MR.Patterns.Repository
 
 		public virtual Task SaveChangesAsync() => Context.SaveChangesAsync();
 
-		public virtual Task RunInTransactionAsync(Func<Task> action)
-			=> RunInTransactionAsync((_) => action());
-
-		public virtual async Task RunInTransactionAsync(Func<IDbTransaction, Task> action)
+		public void RunInTransaction(Action<DbConnection, DbTransaction> action, IsolationLevel? isolationLevel = null)
 		{
-			using (var transaction = Database.BeginTransaction())
+			RunInTransaction((connection, transaction) =>
 			{
-				try
-				{
-					await action(transaction.UnderlyingTransaction);
-
-					await Context.SaveChangesAsync();
-					transaction.Commit();
-				}
-				catch
-				{
-					transaction.Rollback();
-					throw;
-				}
-			}
+				action(connection, transaction);
+				return true;
+			}, null);
 		}
 
-		public virtual Task RunInTransactionAsync(Action action)
-			=> RunInTransactionAsync((_) => action());
-
-		public virtual async Task RunInTransactionAsync(Action<IDbTransaction> action)
+		public Task RunInTransactionAsync(Func<DbConnection, DbTransaction, Task> func, IsolationLevel? isolationLevel = null)
 		{
-			using (var transaction = Database.BeginTransaction())
+			return RunInTransactionAsync(async (connection, transaction) =>
+			{
+				await func(connection, transaction);
+				return true;
+			}, null);
+		}
+
+		public T RunInTransaction<T>(Func<DbConnection, DbTransaction, T> func, IsolationLevel? isolationLevel = null)
+		{
+			T result;
+			using (var contextTransaction = CreateTransaction(isolationLevel))
 			{
 				try
 				{
-					action(transaction.UnderlyingTransaction);
-
-					await Context.SaveChangesAsync();
-					transaction.Commit();
+					result = func(Database.Connection, contextTransaction.UnderlyingTransaction);
+					contextTransaction.Commit();
 				}
 				catch
 				{
-					transaction.Rollback();
+					contextTransaction.Rollback();
 					throw;
 				}
 			}
+			return result;
+		}
+
+		public async Task<T> RunInTransactionAsync<T>(Func<DbConnection, DbTransaction, Task<T>> func, IsolationLevel? isolationLevel = null)
+		{
+			T result;
+			using (var contextTransaction = CreateTransaction(isolationLevel))
+			{
+				try
+				{
+					result = await func(Database.Connection, contextTransaction.UnderlyingTransaction);
+					contextTransaction.Commit();
+				}
+				catch
+				{
+					contextTransaction.Rollback();
+					throw;
+				}
+			}
+			return result;
+		}
+
+		private DbContextTransaction CreateTransaction(IsolationLevel? isolationLevel)
+		{
+			return
+				isolationLevel == null ?
+				Database.BeginTransaction() :
+				Database.BeginTransaction(isolationLevel.Value);
 		}
 
 		public virtual void Dispose()
 		{
 			Context.Dispose();
+		}
+
+		// Obsolete
+
+		[Obsolete("Use the other overloads with manually calling SaveChangesAsync when necessary.")]
+		public virtual Task RunInTransactionAsync(Func<Task> action)
+			=> RunInTransactionAsync((_) => action());
+
+		[Obsolete("Use the other overloads with manually calling SaveChangesAsync when necessary.")]
+		public virtual Task RunInTransactionAsync(Func<IDbTransaction, Task> action)
+		{
+			return RunInTransactionAsync(async (_, transaction) =>
+			{
+				await action(transaction);
+				await SaveChangesAsync();
+			});
+		}
+
+		[Obsolete("Use the other overloads with manually calling SaveChangesAsync when necessary.")]
+		public virtual Task RunInTransactionAsync(Action action)
+			=> RunInTransactionAsync((_) => action());
+
+		[Obsolete("Use the other overloads with manually calling SaveChangesAsync when necessary.")]
+		public virtual Task RunInTransactionAsync(Action<IDbTransaction> action)
+		{
+			return RunInTransactionAsync((_, transaction) =>
+			{
+				action(transaction);
+				return SaveChangesAsync();
+			});
 		}
 	}
 }
