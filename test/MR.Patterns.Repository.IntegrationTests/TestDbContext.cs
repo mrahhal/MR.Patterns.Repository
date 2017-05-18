e4293335df8cb2eb86c8815ca788b05fe210e506
Mohammad Rahhal <m.r992@hotmail.com>
2017-05-18 13:18:23 +0300

    Setup database tests to work on CI

--- test/MR.Patterns.Repository.IntegrationTests/TestDbContext.cs
+++ test/MR.Patterns.Repository.IntegrationTests/TestDbContext.cs
@@ -1,11 +1,12 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
+using System.Data.SqlClient;
 
 namespace MR.Patterns.Repository
 {
 	public class TestDbContext : DbContext
 	{
-		public static string ConnectionString { get; set; }
-			= "Server=(localdb)\\mssqllocaldb;Database=MR.Patterns.Repository.IntegrationTests;Trusted_Connection=True;MultipleActiveResultSets=true";
+		public static string ConnectionString { get; set; } = ConnectionUtil.GetConnectionString();
 
 		public TestDbContext() : base(ConnectionString)
 		{
@@ -14,5 +15,34 @@ namespace MR.Patterns.Repository
 		public DbSet<Blog> Blogs { get; set; }
 
 		public DbSet<Post> Posts { get; set; }
+	}
+
+	public static class ConnectionUtil
+	{
+		private const string ConnectionStringTemplateVariable = "Jobs_SqlServer_ConnectionStringTemplate";
+
+		private const string DefaultDatabaseName = @"MR.Patterns.Repository.IntegrationTests";
+
+		private const string DefaultConnectionStringTemplate = "Server=(localdb)\\mssqllocaldb;Database={0};Trusted_Connection=True;";
+
+		public static string GetConnectionString()
+		{
+			return string.Format(GetConnectionStringTemplate(), DefaultDatabaseName);
+		}
+
+		private static string GetConnectionStringTemplate()
+		{
+			return
+				Environment.GetEnvironmentVariable(ConnectionStringTemplateVariable) ??
+				DefaultConnectionStringTemplate;
+		}
+
+		public static SqlConnection CreateConnection(string connectionString = null)
+		{
+			connectionString = connectionString ?? GetConnectionString();
+			var connection = new SqlConnection(connectionString);
+			connection.Open();
+			return connection;
+		}
 	}
 }
